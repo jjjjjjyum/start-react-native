412f211a5a40aafb361a8a648c7c58d099c57c4c
jjjjjjym <jrdbal@naver.com>
2025-01-02 13:42:48 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Text, View, Button } from "react-native";
+import Mybutton from "./components/MyButton";
+const App = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: "#fff",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Text style={{ fontSize: 30, marginBottom: 10 }}>Props</Text>
+            <Mybutton title="Button"/>
+            <Mybutton>Childeren Props</Mybutton>
+            <Mybutton />
+        </View>
+    );
+};
+
+export default App
---
+++ src/components/MyButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {TouchableOpacity, Text} from 'react-native';
+import PropTypes from 'prop-types';
+
+const Mybutton = ({title = 'default', ...props}) => {
+    // props.title = 'ooo'; 
+    console.log(props);
+    return (
+        <TouchableOpacity
+            style={{
+                backgroundColor:'#3498db',
+                padding:16,
+                margin:10,
+                borderRadius:8,
+            }}
+            onPress={()=>alert('Click!!')}
+        >
+            <Text style={{color:'white',fontSize:24}}>
+                {props.children || title}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+Mybutton.propTypes = {
+    title:PropTypes.string,
+};
+
+export default Mybutton;
